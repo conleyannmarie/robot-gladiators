4442cfa9f5852836cba963544582fe0ff4da2866
Conley Marie <81729253+conleyannmarie@users.noreply.github.com>
2021-08-02 21:09:49 -0600

    added console logs

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
-window.alert("This is an alert! JavaScript is running!");
+window.prompt("What is your robot's name?");
+
+var playerName = window.prompt("What is your robot's name?")
+console.log(playerName);
+console.log("this logs a string, good for leaving yourself a messag")
+//this will do math and log 
+console.log(10+10);
+//what is this
+console.log("Our robot's name is" + playerName)
+
+//fight function //
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
